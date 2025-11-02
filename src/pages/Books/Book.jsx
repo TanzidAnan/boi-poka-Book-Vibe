838eb8ddfee9443aa6a5d2e3d786d8329384f1be
TanzidAnan <tanzid.anan125@gmail.com>
2025-11-02 20:23:41 +0600

    card style

--- src/pages/Books/Book.jsx
+++ src/pages/Books/Book.jsx
@@ -1,26 +1,27 @@
 // import React, { use } from 'react';
+import { FaStar } from "react-icons/fa6";
 
 const Book = ({ singleBook }) => {
     // const data =use(bookPromise);
     console.log(singleBook);
-    const {image} =singleBook
+    const {image,totalPages,author,bookName,rating,category} =singleBook
     return (
         <div>
-            <div className="card  w-96 bg-emerald-50 shadow-sm">
-                <figure className="p-3">
+            <div className="card  w-96 bg-emerald-50 shadow-sm p-2.5">
+                <figure className="p-3 bg-amber-50 rounded-lg">
                     <img className="h-[30vh] w-[60%] lg:w-[40%] rounded"
                         src={image}
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        Card Title
+                        {bookName}
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                     <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
+                        <div className="badge badge-outline">{category}</div>
+                        <div className="badge badge-outline">{rating} <FaStar /></div>
                     </div>
                 </div>
             </div>
@@ -29,3 +30,5 @@ const Book = ({ singleBook }) => {
 };
 
 export default Book;
+
+
